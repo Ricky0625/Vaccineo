2f95a16044d56f1f08798f2096cd577548bd95f1
Ricky <rickywong0625@gmail.com>
2021-11-10 09:01:15 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vaccineo
+
+OODJ Assignment 2021
+
+APU2F2109SE
---
+++ src/vaccineo/Vaccineo.java
@@ -0,0 +1,9 @@
+package vaccineo;
+
+public class Vaccineo {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
