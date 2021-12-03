30dd1b8e49107dff8232e29bbe3d222f782255da
jadson <dexter_winyip01@hotmail.com>
2021-12-03 10:47:45 +0800

    People Classes Done

--- Vaccineo/src/classes/People.java
+++ Vaccineo/src/classes/People.java
@@ -1,12 +1,21 @@
 package classes;
 
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class People {
 
@@ -24,46 +33,293 @@ public class People {
     private String vaccinationStatus;
     private String category;
     private String data;
+    private String value;
+    private String oldaddress;
     private ArrayList<ArrayList<String>> peopleList;
 
-    public People() {
+    public People() { 
         username = "";
         password = "";
+        id = "";
         name = "";
         dob = null;
-        street = "";
-        state = "";
+        street = "-";
+        state = "-";
         country = "";
-        postcode = "";
-        address = "";
+        postcode = "-";
+        address = "-";
         gender = "";
         category = "";
         vaccinationStatus = "";
+        oldaddress = "-";
         data = "people.txt";
     }
 
     public People(String id) {
         this.id = id;
     }
-
-    private void registerPeople() {
-
+    
+    public boolean validatePass(String password){
+        if(password.length() > 7){
+            if(checkPass(password)){
+                return true;
+            } else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+    
+    public boolean checkPass(String password){       
+        boolean hasNum = false; boolean hasCap = false; boolean hasLow = false; char c;
+        for (int i=0; i<password.length(); i++){
+            c = password.charAt(i);
+            if(Character.isDigit(c)){
+                hasNum = true;
+            } else if(Character.isUpperCase(c)){
+                hasCap = true;
+            } else if(Character.isLowerCase(c)){
+                hasLow = true;
+            } if(hasNum && hasCap && hasLow){
+                return true;
+            }
+        } 
+        return false;
     }
 
-    private void peopleLogin() {
-
+    private static Scanner x;
+    public void registerPeople(String username, String name, String id, String address, String state, String country, String postcode, 
+                               String password, String gender, String category, String dob) {
+        boolean found = false;
+        
+        try {
+            x = new Scanner(new File(data));
+            x.useDelimiter("[;]");
+            
+            while(x.hasNext() && !found){
+                String icpass = x.next();
+                
+                if(id.equals(icpass)){
+                    found = true;
+                }
+            }
+            if(found){
+                JOptionPane.showMessageDialog(null, "Existing IC numbers");
+            } else {
+                try {
+                    String vaccinestatus = "Not Registered";
+                    BufferedWriter bw = new BufferedWriter(new FileWriter(data, true));
+                    PrintWriter pw = new PrintWriter(bw);
+                    pw.println(""+username+";"+password+";"+id+";"+name+";"+dob+";"+address+";"+state+";"+country+";"+postcode+";"+gender+";"+category+";"+vaccinestatus);
+                    pw.flush();
+                    pw.close();
+                    bw.close();
+                    JOptionPane.showMessageDialog(null, "Account Registered Successfully");
+                }
+                catch(IOException e){
+                    System.out.println("Fail to register your account");
+                }
+            }
+        } catch(Exception e){
+            System.out.println("Error");
+        }
+    }
+    
+    private static Scanner y;
+    public void registerPeopleNon(String username, String name, String id, String address, 
+                               String password, String gender, String category, String dob) {
+        boolean found = false;
+        
+        try {
+            y = new Scanner(new File(data));
+            y.useDelimiter("[;]");
+            
+            while(y.hasNext() && !found){
+                String icpass = y.next();
+                
+                if(id.equals(icpass)){
+                    found = true;
+                }
+            }
+            if(found){
+                JOptionPane.showMessageDialog(null, "Existing IC numbers");
+            } else {
+                try {
+                    String vaccinestatus = "Not Registered";
+                    BufferedWriter bw = new BufferedWriter(new FileWriter(data, true));
+                    PrintWriter pw = new PrintWriter(bw);
+                    pw.println(""+username+";"+password+";"+id+";"+name+";"+dob+";"+oldaddress+";"+state+";"+address+";"+postcode+";"+gender+";"+category+";"+vaccinestatus);
+                    pw.flush();
+                    pw.close();
+                    bw.close();
+                    JOptionPane.showMessageDialog(null, "Account Registered Successfully");
+                }
+                catch(IOException e){
+                    System.out.println("Fail to register your account");
+                }
+            }
+        } catch(Exception e){
+            System.out.println("Error");
+        }
+    }
+         
+    public boolean peopleLogin(String username, String password) {
+        boolean found = false;
+        File file = new File(data);
+        
+        try {
+            Scanner readfile = new Scanner(file);
+            
+            while(readfile.hasNext()){
+                String line = readfile.nextLine();
+                String[] values = line.split(";");
+                
+                if(values[0].equals(username)){
+                    if(values[1].equals(password)){
+                        found = true;
+                        String id = values[2];
+                        setId(id);
+                    } 
+                } 
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("An error occurred.");
+        }
+        return found;
     }
 
-    private void modifyInfo() {
+    public void modifyInfo(String value, String username, String address, String state, String country, String postcode) {
+        boolean found = false;
+        ArrayList<String> tempArray = new ArrayList<>();
+        try {
+            try (FileReader fr = new FileReader(data)) {
+                Scanner reader = new Scanner(fr);
+                String line;
+                String[] lineArr;
+                while ((line = reader.nextLine()) != null) {
+                    lineArr = line.split(";");
 
+                    if (lineArr[2].equals(value)) {
+                        found = true;
+                        tempArray.add(
+                                username + ";"
+                                + lineArr[1] + ";"
+                                + lineArr[2] + ";"
+                                + lineArr[3] + ";"
+                                + lineArr[4] + ";"
+                                + address + ";"
+                                + state + ";"
+                                + country + ";"
+                                + postcode + ";"
+                                + lineArr[9] + ";"
+                                + lineArr[10] + ";"
+                                + lineArr[11] );
+                        JOptionPane.showMessageDialog(null, "Edit Profile Successfully");
+
+                    } else {
+                        tempArray.add(line);
+                    }
+                }
+                fr.close();
+            } catch (Exception e) {
+
+            }
+        } catch (Exception e) {
+
+        }
+        try {
+            try (PrintWriter pr = new PrintWriter("people.txt")) {
+                for (String str : tempArray) {
+                    pr.println(str);
+                }
+                pr.close();
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error");
+        }
     }
 
-    private void viewPeopleInfo() {
+    public void viewPeopleInfo(String value) {
+    }
+    
+    public void peopleDeclaration(String name, String id, String q1, String q2, String q3, String q4,
+                                  String q5, String q6, String q7, String q8){
+        
+        String declafile = "declaration.txt";
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(declafile, true));
+            PrintWriter pw = new PrintWriter(bw);
+            pw.println(""+id+";"+name+";"+q1+";"+q2+";"+q3+";"+q4+";"+q5+";"+q6+";"+q7+";"+q8);
+            pw.flush();
+            pw.close();
+            bw.close();
+            JOptionPane.showMessageDialog(null, "Vaccine Register Successfully");
+        }
+        catch(IOException e){
+            System.out.println("Error");
+        }
+    }
+    
+    public void updateNewStatus(String id, String newVacStatus){
+        boolean found = false;
+        ArrayList<String> tempArray = new ArrayList<>();
+        try {
+            try (FileReader fr = new FileReader(data)) {
+                Scanner reader = new Scanner(fr);
+                String line;
+                String[] lineArr;
+                while ((line = reader.nextLine()) != null) {
+                    lineArr = line.split(";");
 
+                    if (lineArr[2].equals(id)) {
+                        found = true;
+                        tempArray.add(
+                                lineArr[0] + ";"
+                                + lineArr[1] + ";"
+                                + lineArr[2] + ";"
+                                + lineArr[3] + ";"
+                                + lineArr[4] + ";"
+                                + lineArr[5] + ";"
+                                + lineArr[6] + ";"
+                                + lineArr[7] + ";"
+                                + lineArr[8] + ";"
+                                + lineArr[9] + ";"
+                                + lineArr[10] + ";"
+                                + newVacStatus );
+                        System.out.println("Status Update Successfully");
+                    } else {
+                        tempArray.add(line);
+                    }
+                }
+                fr.close();
+            } catch (Exception e) {
+
+            }
+        } catch (Exception e) {
+
+        }
+        try {
+            try (PrintWriter pr = new PrintWriter("people.txt")) {
+                for (String str : tempArray) {
+                    pr.println(str);
+                }
+                pr.close();
+
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Error");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error");
+        }
     }
 
     public void checkEligible() {
-
     }
 
     public void setId(String id) {
@@ -80,6 +336,22 @@ public class People {
 
     public String getName() {
         return name;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+       
+    public void setPassword(String password) {
+        this.name = password;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public void setDOB(String dob) throws ParseException {
@@ -193,6 +465,7 @@ public class People {
                 // search using id
                 String searchId = list.get(i).get(2);
                 if (searchId.equals(id)) {
+                    setUsername(list.get(i).get(0));
                     setId(list.get(i).get(2));
                     setName(list.get(i).get(3));
                     try {
